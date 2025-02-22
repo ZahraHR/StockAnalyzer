ebc1876c0e8f23e940b99fe2ad20c7b8b9af02f8
Fatima Harti <fatima.harti@cegedim.com>
2025-02-22 18:13:12 +0100

    Tweets Analysis: Visualization options

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import pandas as pd
 from io import StringIO
 
 # Importer les modules utils
-from utils.preprocessing import preprocess
+from utils.preprocessing import preprocess, basic_cleaning, dslim_bert_ner_get_ent
 from utils.twitter_api import fetch_tweets
-from utils.visualization import generate_wordcloud
+from utils.visualization import generate_wordcloud, plot_top_orgs
 
 st.sidebar.title("Stock Analysis")
 option = st.sidebar.radio("What do you want to analyze", ("Twitter Sentiment Analysis","Other"))
@@ -20,12 +20,14 @@ if option == "Twitter Sentiment Analysis":
 
         if uploaded_file is not None:
             data = StringIO(uploaded_file.getvalue().decode("utf-8"))
-            df = pd.read_csv(data)
-            st.write(df.head())
+            tweet_df = pd.read_csv(data)
+            st.write(tweet_df.head())
 
-            num_rows = st.slider("How many rows to display?", min_value=1, max_value=len(df), value=5)
+            st.session_state.df = tweet_df
+
+            num_rows = st.slider("How many rows to display?", min_value=1, max_value=len(tweet_df), value=5)
             st.write(f"Displaying {num_rows} rows of data:")
-            st.write(df.head(num_rows))
+            st.write(tweet_df.head(num_rows))
 
     elif upload_option == "Fetch Tweets":
         st.title("Fetch Tweets from Twitter")
@@ -37,7 +39,33 @@ if option == "Twitter Sentiment Analysis":
                 tweet_df = pd.DataFrame(tweet_data)
                 st.write(tweet_df.head())
 
+                st.session_state.df = tweet_df
+
                 num_rows = st.slider("How many rows to display?", min_value=1, max_value=len(tweet_df), value=5)
                 st.write(f"Displaying {num_rows} rows of data:")
                 st.write(tweet_df.head(num_rows))
+
+    if 'df' in st.session_state and st.session_state.df is not None:
+        st.sidebar.title("Visualize data")
+        visualize_option = st.sidebar.radio("Choose an option", ("Word Cloud", "Bar Plot"))
+
+        # Word Cloud Visualization
+        if visualize_option == "Word Cloud":
+            st.title("Word Cloud")
+            tweet_df = st.session_state.df  # Use the tweet data from session state
+            tweet_df["processed_text"] = tweet_df["text"].apply(preprocess)
+
+            # Generate and display the Word Cloud
+            generate_wordcloud(tweet_df["processed_text"])
+        elif visualize_option == "Bar Plot":
+            st.title("Top Organizations Bar Plot")
+
+            tweet_df["cleaned_text"] = tweet_df["text"].apply(lambda x: basic_cleaning(x))
+            tweet_df["bert_orgs"] = tweet_df.cleaned_text.apply(lambda x: dslim_bert_ner_get_ent(x))
+
+            df_top, fig = plot_top_orgs(tweet_df["bert_orgs"], top_n=5)
+
+            st.write(df_top)
+            st.plotly_chart(fig)
+
 # streamlit run app.py
--- utils/visualization.py
+++ utils/visualization.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import streamlit as st
-
+import plotly.express as px
+from collections import Counter
+from itertools import chain
+import pandas as pd
 
 def generate_wordcloud(texts):
     """Créer et afficher un wordcloud avec Matplotlib"""
@@ -12,3 +15,29 @@ def generate_wordcloud(texts):
     ax.axis("off")
 
     st.pyplot(fig)
+
+
+def plot_top_orgs(data, top_n=10):
+    """
+    Retourne un DataFrame des organisations les plus fréquentes et un graphique interactif.
+
+    Paramètres :
+    - data (pd.Series) : Série contenant des listes d'organisations.
+    - top_n (int) : Nombre d'organisations à afficher (par défaut 10).
+
+    Retour :
+    - df_top_counts (pd.DataFrame) : DataFrame des organisations les plus fréquentes.
+    - fig (plotly.graph_objects.Figure) : Graphique interactif Plotly.
+    """
+    all_values = list(chain(*data.apply(lambda x: list(set(x))).values))
+    value_counts = Counter(all_values)
+
+    df_counts = pd.DataFrame(value_counts.items(), columns=["Value", "Count"]).sort_values(by="Count", ascending=False)
+
+    df_top_counts = df_counts.head(top_n)
+
+    fig = px.bar(df_top_counts, x="Value", y="Count", title=f"Top {top_n} Organisations par Count",
+                 labels={"Value": "Organisation", "Count": "Nombre d'occurrences"},
+                 text_auto=True)
+
+    return df_top_counts, fig
