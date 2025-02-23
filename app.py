09ae515330019146d4cc9f2bcb729fafe05decff
Fatima Harti <fatima.harti@cegedim.com>
2025-02-23 16:06:37 +0100

    add finBert prediction fonctionality

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from io import StringIO
 # Importer les modules utils
 from utils.preprocessing import preprocess, basic_cleaning, dslim_bert_ner_get_ent
 from utils.twitter_api import fetch_tweets
-from utils.visualization import generate_wordcloud, plot_top_orgs
+from utils.visualization import generate_wordcloud, plot_top_orgs, plot_pie_chart
+
+from transformers import pipeline
 
 st.sidebar.title("Stock Analysis")
 option = st.sidebar.radio("What do you want to analyze", ("Twitter Sentiment Analysis","Other"))
@@ -21,7 +23,7 @@ if option == "Twitter Sentiment Analysis":
         if uploaded_file is not None:
             data = StringIO(uploaded_file.getvalue().decode("utf-8"))
             tweet_df = pd.read_csv(data)
-            st.write(tweet_df.head())
+            #st.write(tweet_df.head())
 
             st.session_state.df = tweet_df
 
@@ -37,7 +39,7 @@ if option == "Twitter Sentiment Analysis":
             tweet_data = fetch_tweets(query)
             if tweet_data:
                 tweet_df = pd.DataFrame(tweet_data)
-                st.write(tweet_df.head())
+                #st.write(tweet_df.head())
 
                 st.session_state.df = tweet_df
 
@@ -46,26 +48,42 @@ if option == "Twitter Sentiment Analysis":
                 st.write(tweet_df.head(num_rows))
 
     if 'df' in st.session_state and st.session_state.df is not None:
-        st.sidebar.title("Visualize data")
-        visualize_option = st.sidebar.radio("Choose an option", ("Word Cloud", "Bar Plot"))
+        st.sidebar.title("Analysis")
+        upload_option = st.sidebar.radio("Choose an option", ("Visualization", "Predictions"))
 
-        # Word Cloud Visualization
-        if visualize_option == "Word Cloud":
-            st.title("Word Cloud")
-            tweet_df = st.session_state.df  # Use the tweet data from session state
-            tweet_df["processed_text"] = tweet_df["text"].apply(preprocess)
+        if upload_option == "Visualization":
 
-            # Generate and display the Word Cloud
-            generate_wordcloud(tweet_df["processed_text"])
-        elif visualize_option == "Bar Plot":
-            st.title("Top Organizations Bar Plot")
+            st.sidebar.title("Visualize data")
+            visualize_option = st.sidebar.radio("Choose an option", ("Word Cloud", "Bar Plot"))
 
+            if visualize_option == "Word Cloud":
+                st.title("Word Cloud")
+                tweet_df = st.session_state.df
+                tweet_df["processed_text"] = tweet_df["text"].apply(preprocess)
+
+                generate_wordcloud(tweet_df["processed_text"])
+            elif visualize_option == "Bar Plot":
+
+                tweet_df["cleaned_text"] = tweet_df["text"].apply(lambda x: basic_cleaning(x))
+                tweet_df["bert_orgs"] = tweet_df.cleaned_text.apply(lambda x: dslim_bert_ner_get_ent(x))
+
+                df_top, fig = plot_top_orgs(tweet_df["bert_orgs"], top_n=5)
+
+                st.write(df_top)
+                st.plotly_chart(fig)
+
+        if upload_option == "Predictions":
             tweet_df["cleaned_text"] = tweet_df["text"].apply(lambda x: basic_cleaning(x))
-            tweet_df["bert_orgs"] = tweet_df.cleaned_text.apply(lambda x: dslim_bert_ner_get_ent(x))
 
-            df_top, fig = plot_top_orgs(tweet_df["bert_orgs"], top_n=5)
+            finbert = pipeline('sentiment-analysis', model='ProsusAI/finbert')
+            tweet_df["polarity_predictions"] = tweet_df["cleaned_text"].apply(lambda x:finbert(x)[0]['label'])
 
-            st.write(df_top)
+            fig = plot_pie_chart(tweet_df["polarity_predictions"])
             st.plotly_chart(fig)
+
+
+
+
+
 
 # streamlit run app.py
