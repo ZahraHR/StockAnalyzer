238f9b181c238f01d5dcce28ab33ce3d3932e902
Fatima Harti <fatimaharti.insea@gmail.com>
2025-02-23 16:38:47 +0100

    Sentiment Analysis - predictions : add multibar polarities by company

--- app.py
+++ app.py
@@ -5,85 +5,65 @@ from io import StringIO
 # Importer les modules utils
 from utils.preprocessing import preprocess, basic_cleaning, dslim_bert_ner_get_ent
 from utils.twitter_api import fetch_tweets
-from utils.visualization import generate_wordcloud, plot_top_orgs, plot_pie_chart
-
+from utils.visualization import generate_wordcloud, plot_top_orgs, plot_pie_chart, plot_top5_sentiment_multibar
 from transformers import pipeline
 
+
+finbert = pipeline('sentiment-analysis', model='ProsusAI/finbert')
+
+
 st.sidebar.title("Stock Analysis")
-option = st.sidebar.radio("What do you want to analyze", ("Twitter Sentiment Analysis","Other"))
+option = st.sidebar.radio("What do you want to analyze", ("Twitter Sentiment Analysis", "Other"))
 
-if option == "Twitter Sentiment Analysis":
-    st.sidebar.title("Upload or Fetch Tweets")
-    upload_option = st.sidebar.radio("Choose an option", ("Upload CSV", "Fetch Tweets"))
 
-    if upload_option == "Upload CSV":
-        st.title("Upload CSV to Process Data")
+def load_tweets():
+    tweet_df = None
+    if st.sidebar.radio("Choose an option", ("Upload CSV", "Fetch Tweets")) == "Upload CSV":
         uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-
         if uploaded_file is not None:
             data = StringIO(uploaded_file.getvalue().decode("utf-8"))
             tweet_df = pd.read_csv(data)
-            #st.write(tweet_df.head())
-
-            st.session_state.df = tweet_df
-
-            num_rows = st.slider("How many rows to display?", min_value=1, max_value=len(tweet_df), value=5)
-            st.write(f"Displaying {num_rows} rows of data:")
-            st.write(tweet_df.head(num_rows))
-
-    elif upload_option == "Fetch Tweets":
-        st.title("Fetch Tweets from Twitter")
+    elif st.sidebar.radio("Choose an option", ("Upload CSV", "Fetch Tweets")) == "Fetch Tweets":
         query = st.text_input("Enter your query (e.g., 'Tech Stocks -is:retweet lang:en')", "Tech Stocks -is:retweet lang:en")
-
         if query:
             tweet_data = fetch_tweets(query)
             if tweet_data:
                 tweet_df = pd.DataFrame(tweet_data)
-                #st.write(tweet_df.head())
+    return tweet_df
 
-                st.session_state.df = tweet_df
+if option == "Twitter Sentiment Analysis":
+    tweet_df = load_tweets()
 
-                num_rows = st.slider("How many rows to display?", min_value=1, max_value=len(tweet_df), value=5)
-                st.write(f"Displaying {num_rows} rows of data:")
-                st.write(tweet_df.head(num_rows))
+    if tweet_df is not None:
+        st.session_state.df = tweet_df
+        num_rows = st.slider("How many rows to display?", min_value=1, max_value=len(tweet_df), value=5)
+        st.write(f"Displaying {num_rows} rows of data:")
+        st.write(tweet_df.head(num_rows))
 
-    if 'df' in st.session_state and st.session_state.df is not None:
-        st.sidebar.title("Analysis")
-        upload_option = st.sidebar.radio("Choose an option", ("Visualization", "Predictions"))
+        analysis_option = st.sidebar.radio("Choose an option", ("Visualization", "Predictions"))
 
-        if upload_option == "Visualization":
-
-            st.sidebar.title("Visualize data")
+        if analysis_option == "Visualization":
             visualize_option = st.sidebar.radio("Choose an option", ("Word Cloud", "Bar Plot"))
-
             if visualize_option == "Word Cloud":
                 st.title("Word Cloud")
-                tweet_df = st.session_state.df
                 tweet_df["processed_text"] = tweet_df["text"].apply(preprocess)
-
                 generate_wordcloud(tweet_df["processed_text"])
             elif visualize_option == "Bar Plot":
-
                 tweet_df["cleaned_text"] = tweet_df["text"].apply(lambda x: basic_cleaning(x))
                 tweet_df["bert_orgs"] = tweet_df.cleaned_text.apply(lambda x: dslim_bert_ner_get_ent(x))
-
                 df_top, fig = plot_top_orgs(tweet_df["bert_orgs"], top_n=5)
-
                 st.write(df_top)
                 st.plotly_chart(fig)
 
-        if upload_option == "Predictions":
+        elif analysis_option == "Predictions":
             tweet_df["cleaned_text"] = tweet_df["text"].apply(lambda x: basic_cleaning(x))
-
-            finbert = pipeline('sentiment-analysis', model='ProsusAI/finbert')
-            tweet_df["polarity_predictions"] = tweet_df["cleaned_text"].apply(lambda x:finbert(x)[0]['label'])
+            tweet_df["polarity_predictions"] = tweet_df["cleaned_text"].apply(lambda x: finbert(x)[0]['label'])
 
             fig = plot_pie_chart(tweet_df["polarity_predictions"])
             st.plotly_chart(fig)
 
-
-
-
-
+            tweet_df["bert_orgs"] = tweet_df.cleaned_text.apply(lambda x: dslim_bert_ner_get_ent(x))
+            fig = plot_top5_sentiment_multibar(tweet_df)
+            st.plotly_chart(fig)
 
 # streamlit run app.py
